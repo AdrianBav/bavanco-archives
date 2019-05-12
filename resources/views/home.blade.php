181a0067b14ff932ceb0903209746ad39aa40e67
Adrian Bavister <adrian@bavanco.co.uk>
2019-05-11 21:22:45 -0500

    Added main content

---
+++ resources/views/home.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="These archives aim to preserve a definitive history of the Bavanco branding and online web presence through the years. Other Bavanco side projects are also archived here for completeness.">
+
+        <title>The Archives | Bavanco</title>
+        <link rel="icon" type="image/x-icon" href="favicon.ico" />
+
+        <!-- Bootstrap (CDN) -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <!-- Bootstrap Theme (CDN) -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/darkly/bootstrap.min.css" rel="stylesheet" integrity="sha256-IsefKVCcMUlgpXQgIMRsqbIqC6aP153JkybxTa7W7/8= sha512-mCNEsmR1i3vWAq8hnHfbCCpc6V5fP9t0N1fEZ1wgEPF/IKteFOYZ2uk7ApzMXkT71sDJ00f9+rVMyMyBFwsaQg==" crossorigin="anonymous">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+
+    <body>
+        <div class="container">
+
+            <div class="page-header">
+                <h1>The Archives</h1>
+                <p class="lead">These archives aim to preserve a definitive history of the Bavanco branding and online web presence through the years. Other Bavanco side projects are also archived here for completeness.</p>
+            </div>
+
+            <!-- Screaming Bears -->
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <h3 class="panel-title">screaming_bears - <small>http://www.geocities.com/screaming_bears/</small></h3>
+                    <small>These sites are literally last century and existed on geocities.</small>
+                </div>
+                <div class="panel-body">
+                    <ul>
+                        <li><a href="storage/screamingbears/underconstruction/" target="_blank">Screaming Bears: Under Construction</a></li>
+                        <li><a href="storage/screamingbears/version1/" target="_blank">Screaming Bears: Version 1</a></li>
+                        <li><a href="storage/screamingbears/version2/" target="_blank">Screaming Bears: Version 2</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <!-- Bavanco -->
+            <div class="panel panel-danger">
+                <div class="panel-heading">
+                    <h3 class="panel-title">bavanco - <small>http://www.bavanco.co.uk/</small></h3>
+                    <small>Born at the turn of the century and still going strong!</small>
+                </div>
+                <div class="panel-body">
+                    <ul>
+                        <li><a href="storage/bavanco/version2001/" target="_blank">Bavanco: Version 2001</a></li>
+                        <li><a href="storage/bavanco/version2002/" target="_blank">Bavanco: Version 2002</a></li>
+                        <li><a href="storage/bavanco/underconstruction/" target="_blank">Bavanco: Under Construction</a></li>
+                        <li><a href="storage/bavanco/version2003/" target="_blank">Bavanco: Version 2003</a></li>
+                        <li><a href="storage/bavanco/version2008/" target="_blank">Bavanco: Version 2008</a></li>
+                        <li><a href="storage/bavanco/comingsoon/" target="_blank">Bavanco: Coming Soon</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <!-- KittyScampers -->
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title">kittyscampers - <small>http://www.kittyscampers.info/</small></h3>
+                    <small>Had a brief 2 year run in 2009.</small>
+                </div>
+                <div class="panel-body">
+                    <ul>
+                        <li><a href="storage/kittyscampers/" target="_blank">Kittyscampers.nut</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <footer>
+                <hr>
+                <p>Copyright © 2002 - <?php echo date('Y'); ?> Bavanco</p>
+            </footer>
+
+        </div>
+    </body>
+
+</html>
